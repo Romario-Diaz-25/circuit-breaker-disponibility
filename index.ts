363d8af6d8e756dbd323cd8f2f3f546ace599988
Romario-Diaz-25 <romario.diaz@globals.one>
2025-03-06 22:44:08 -0500

    addding excecutor of circuit breaker

--- index.ts
+++ index.ts
@@ -1,26 +1,5 @@
-console.log("Hello World");
+import { server } from "./src/server";
 
-// import fs from "fs";
-// import { env } from "./src/infrasctructure/config/env/environments";
-// import { server } from "./src/server";
-// import { checkEnvVariables } from "./src/utils/check-env-varibles.util";
-// import { AppError } from "./src/lib/app-error";
-// import { appConsole } from "./src/utils/app-console";
-
-// const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
-// const apiVersion = packageJson.version;
-
-// process.on("uncaughtException", (error: Error) => {
-//   appConsole.error(error);
-//   const isTrusted = error instanceof AppError;
-//   if (!isTrusted) {
-//     process.exit(1);
-//   }
-// });
-
-// server.listen(env.app.port, () => {
-//   checkEnvVariables(env);
-//   appConsole.log(
-//     `Server running on port ${env.app.port} version: ${apiVersion}`
-//   );
-// });
+server.listen(3001, () => {
+  console.log("Server running on port 3000");
+});
